ac89fff905dcc47d6d5cf73fdff35392e94760b5
Lanxiaozhi <lanxiaozhidoge@gmail.com>
2018-11-27 12:07:19 +0800

    support git url

--- DockerPack/pack.py
+++ DockerPack/pack.py
@@ -5,10 +5,13 @@ import argparse
 client = docker.Client()
 
 
-def create_dockerfile(local_dir_name, work_dir_name, run_cmd):
-    __dockerfile = ['FROM python:3.6 \n', 'WORKDIR {} \n'.format(work_dir_name),
-                    'COPY {} ./ \n'.format(local_dir_name), 'RUN pip install -r ./requirements.txt \n',
-                    'CMD {}~'.format(run_cmd)]
+def create_dockerfile(local_dir_name, git_url, work_dir_name, run_cmd):
+    __dockerfile = ['FROM python:3.6 \n', 'WORKDIR {} \n'.format(work_dir_name)]
+    if git_url is not None:
+        __dockerfile.append('RUN git clone {} \n'.format(git_url))
+    __dockerfile.extend([
+        'COPY {} ./ \n'.format(local_dir_name), 'RUN pip install -r ./requirements.txt \n',
+        'CMD {}~'.format(run_cmd)])
     with open("Dockerfile", 'w') as f:
         for cmd in __dockerfile:
             f.write(cmd)
@@ -34,6 +37,7 @@ if __name__ == "__main__":
     parse = argparse.ArgumentParser()
 
     parse.add_argument("-l", "--local")
+    parse.add_argument("-g", "--git", default=None)
     parse.add_argument("-w", "--work")
     parse.add_argument("-r", "--repo", default=None)
     parse.add_argument("-t", "--tag")
@@ -41,7 +45,7 @@ if __name__ == "__main__":
 
     args = parse.parse_args()
 
-    create_dockerfile(local_dir_name=args.local, work_dir_name=args.work, run_cmd=str(args.command))
+    create_dockerfile(local_dir_name=args.local, git_url=args.git, work_dir_name=args.work, run_cmd=str(args.command))
     build(tag=args.tag)
     if args.repo:
         push(tag=args.tag, repo=args.repo)
