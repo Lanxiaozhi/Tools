b3853d9e0feb03ff08ef26eac11caa5502bfd5ab
Lanxiaozhi <lanxiaozhidoge@gmail.com>
2018-11-29 17:15:16 +0800

    use docker-python

--- DockerPack/pack.py
+++ DockerPack/pack.py
@@ -1,19 +1,26 @@
-import os
+# import os
 import docker
+import json
 import argparse
 
 client = docker.from_env()
 
 
-def create_dockerfile(local_dir_name, git_url, work_dir_name, run_cmd):
-    __dockerfile = ['FROM python:3.6 \n']
+def create_dockerfile(local_dir_name, git_url, work_dir_name, run_cmd, env_list=[],
+                      base_image='python:3.6', maintainer='xlearn'):
+    assert local_dir_name is not None or git_url is not None, "local and git is both None!"
+    __dockerfile = ['FROM {} \n'.format(base_image),
+                    'MAINTAINER {} \n'.format(maintainer)]
+    for env in env_list:
+        __dockerfile.append('ENV {} \n'.format(str((" ").join(env.split(",")))))
     if git_url is not None:
         __dockerfile.append('RUN git clone {} \n'.format(git_url))
     else:
-        __dockerfile.append('COPY {} {} \n'.format(local_dir_name, work_dir_name))
+        __dockerfile.append('ADD {} {} \n'.format(local_dir_name, work_dir_name), )
     __dockerfile.extend([
         'WORKDIR {} \n'.format(work_dir_name),
         'RUN pip install -r ./requirements.txt \n'.format(work_dir_name),
+        # 'RUN python3 setup.py install \n',
         'CMD {}~'.format(str(run_cmd.split(",")))])
     with open("Dockerfile", 'w') as f:
         for cmd in __dockerfile:
@@ -21,46 +28,62 @@ def create_dockerfile(local_dir_name, git_url, work_dir_name, run_cmd):
 
 
 def build(tag):
-    """
-    for line in client.build(path='.', tag=tag):
-        log = line.decode('utf-8').split('\r\n')
-        for item in log:
-            if item.find("stream") > -1:
-                print((json.loads(item))['stream'])
-    """
-    os.system("docker build -t {} .".format(tag))
+    return client.images.build(path='.', tag=tag)
+    # os.system("docker build -t {} .".format(tag))
 
 
-def push(tag, repo):
-    image_id = get_image_id(tag=tag)
-    os.system("docker tag {} {}".format(image_id, repo))
-    os.system("docker push {}".format(repo))
+def show_log(log):
+    for line in log:
+        print(line.get("stream"))
+
+
+def push(image, repo, username, password):
+    if username and password:
+        client.login(username=username, password=password)
+    res = image.tag(repo)
+    if not res:
+        print("tag failed!")
+        return
+    else:
+        client.images.push(repository=repo)
+    # image_id = client.images.get(tag).short_id.replace("sha256:", "")
+    # os.system("docker tag {} {}".format(image_id, repo))
+    # os.system("docker push {}".format(repo))
     # client.push(repository=repo)
 
 
+"""
 def get_image_id(tag):
     for item in client.images():
         if tag in item["RepoTags"]:
             return (item["Id"].replace("sha256:", ""))[:12]
+"""
 
 
 def main():
     parse = argparse.ArgumentParser()
 
-    parse.add_argument("-l", "--local")
+    parse.add_argument("-l", "--local", default=None)
     parse.add_argument("-g", "--git", default=None)
+    parse.add_argument("-e", "--envs", nargs='+', default=[])
+    parse.add_argument("-m", "--maintainer", default="xlearn")
+    parse.add_argument("-b", "--base", default="python:3.6")
     parse.add_argument("-w", "--work")
     parse.add_argument("-t", "--tag")
     parse.add_argument("-c", "--command")
 
     parse.add_argument("-r", "--repo", default=None)
+    parse.add_argument("-u", "--username", default=None)
+    parse.add_argument("-p", "--password", default=None)
 
     args = parse.parse_args()
 
-    create_dockerfile(local_dir_name=args.local, git_url=args.git, work_dir_name=args.work, run_cmd=str(args.command))
-    build(tag=args.tag)
+    create_dockerfile(local_dir_name=args.local, git_url=args.git, work_dir_name=args.work, run_cmd=str(args.command),
+                      maintainer=args.maintainer, base_image=args.base, env_list=args.envs)
+    image, log = build(tag=args.tag)
+    show_log(log)
     if args.repo:
-        push(tag=args.tag, repo=args.repo)
+        push(image=image, repo=args.repo, username=args.username, password=args.password)
     client.close()
 
 
